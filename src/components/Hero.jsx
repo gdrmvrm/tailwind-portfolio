1dd3d96999057968f0370a3c52f572b792fe5936
gdrmvrm <gdrmvrm@gmail.com>
2025-11-25 16:51:44 +0000

    set up hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,14 @@
-import heroImg from '../assets/hero.svg';
+import heroImg from '../assets/hero1.svg';
 import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
 
 const Hero = () => {
   return (
     <div className='bg-emerald-100 py-24'>
       <div className='align-element grid md:grid-cols-2 items-center gap-8'>
-        <article>
-          <h1 className='text-7xl font-bold tracking-wider'>I'm Gaya</h1>
+        <article className='align-element'>
+          <h1 className='text-6xl font-bold tracking-wider md:text-7xl'>
+            I'm Gayathri
+          </h1>
           <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
             Front-End Developer
           </p>
@@ -20,13 +22,10 @@ const Hero = () => {
             <a href='#'>
               <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
             </a>
-            <a href='#'>
-              <FaTwitterSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
-            </a>
           </div>
         </article>
         <article className='hidden md:block'>
-          <img src={heroImg} className='h-80 lg:h-96' />
+          <img src={heroImg} className='h-120 lg:h-96' />
         </article>
       </div>
     </div>

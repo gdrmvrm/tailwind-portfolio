3393c2387d7b946c24136d2da8e814a37d963d93
gdrmvrm <gdrmvrm@gmail.com>
2025-11-26 11:45:06 +0000

    projects section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <div class='align-element grid  md:grid-cols-2 items-center gap-16'>
         <img src={aboutSvg} className='w-full h-64' />
         <article>
-          <SectionTitle text='code and coffee' />
+          <SectionTitle text='coder & artist' />
           <p className='text-slate-600 mt-8 leading-loose'>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro
             omnis exercitationem tempora, aliquid deleniti tenetur vero esse
